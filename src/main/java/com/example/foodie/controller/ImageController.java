37e3e128487ed3d9550cba52ee8d1ab060b207d3
Dias Kozhabay <librerush@tutanota.com>
2021-05-11 17:16:50 +0600

    Add upload an image method

--- src/main/java/com/example/foodie/controller/ImageController.java
+++ src/main/java/com/example/foodie/controller/ImageController.java
@@ -5,10 +5,8 @@ import org.apache.commons.io.FileUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.File;
@@ -17,7 +15,7 @@ import java.io.IOException;
 @Controller
 @RequestMapping("/image")
 public class ImageController {
-    private final static String IMAGE_STORAGE_PATH = "data/image/";
+    private final static String IMAGE_STORAGE_PATH = "storage/image/";
 
     @GetMapping(value = "/{path}",
             produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
@@ -32,5 +30,26 @@ public class ImageController {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No image: " + path);
         }
         return bytes;
+    }
+
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @Operation(summary = "Upload a file to storage")
+    public @ResponseBody String uploadImage(@RequestBody MultipartFile multipartFile) {
+        if (multipartFile == null
+                || multipartFile.isEmpty()
+                || multipartFile.getSize() > 10000000L) /* more than 10 MB */ {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        if (!multipartFile.isEmpty()) {
+            String path = IMAGE_STORAGE_PATH + multipartFile.getOriginalFilename();
+            try {
+                multipartFile.transferTo(new File(path).getAbsoluteFile());
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                        "Got an exception when uploading a file: " + multipartFile.getOriginalFilename());
+            }
+        }
+        return "Uploaded a file: " + multipartFile.getOriginalFilename();
     }
 }
